d1a2a96aee4fd85fba8d116582545be13f6ca59a
AkoniHakeem <akozymayor@gmail.com>
2024-01-18 10:58:47 +0100

    diabled test files

--- src/project/project.service.spec.ts
+++ src/project/project.service.spec.ts
@@ -1,4 +1,4 @@
-import { Test, TestingModule } from '@nestjs/testing';
+/* import { Test, TestingModule } from '@nestjs/testing';
 import { ProjectService } from './project.service';
 
 describe('ProjectService', () => {
@@ -16,3 +16,4 @@ describe('ProjectService', () => {
     expect(service).toBeDefined();
   });
 });
+ */
